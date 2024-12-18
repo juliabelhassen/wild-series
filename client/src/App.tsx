64113937bb2e85f795e65f08d7782d90073aac2f
BELHASSEN <julia_belhassen@hotmail.com>
2024-12-18 15:07:35 +0100

    Welcome to wild serie

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <header>
-        <h1 className="logo">JS Monorepo</h1>
+        <h1 className="logo">Welcome to Wild Series</h1>
       </header>
 
       <nav className="navbar">
@@ -34,26 +34,6 @@ function App() {
           </li>
         </ul>
       </nav>
-
-      <main className="text-box">
-        <hgroup className="block-primary">
-          <h2 className="block-primary-main">JS Monorepo</h2>
-          <p className="block-primary-sub">Votre framework JavaScript</p>
-        </hgroup>
-        <p>Vous avez lu le README ?</p>
-      </main>
-
-      <footer>
-        Développé par la&nbsp;
-        <a
-          href="https://www.wildcodeschool.com/"
-          className="wcs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wild Code School
-        </a>
-      </footer>
     </>
   );
 }
